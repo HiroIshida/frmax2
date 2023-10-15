aca291555ffdf8692fc53f5879893543a3fad81d
HiroIshida <spitfire.docomo@gmail.com>
2023-10-15 09:29:22 +0900

    feat: gaussian with hollow

--- frmax2/environment.py
+++ frmax2/environment.py
@@ -10,15 +10,20 @@ def npdf(dist_from_center: float) -> float:
 
 
 class GaussianEnvironment:
-    def __init__(self, n_dim: int, m_dim: int, with_bias: bool = False):
+    def __init__(self, n_dim: int, m_dim: int, with_bias: bool = False, with_hollow: bool = False):
         self.n_dim = n_dim
         self.m_dim = m_dim
         self.name = "gaussian"
-        self.with_bias = with_bias
         if with_bias:
             self.bias_param = 0.8
         else:
             self.bias_param = 0.0
+        if with_hollow:
+            self.hollow_scale = 0.2
+        else:
+            self.hollow_scale = 0.0
+        self.with_hollow = with_hollow
+        self.with_bias = with_bias
 
     def _npdf(self, param):
         dists = np.sqrt(np.sum(param**2))
@@ -37,13 +42,12 @@ class GaussianEnvironment:
 
     def isInside(self, x: np.ndarray) -> bool:
         assert x.ndim == 1, "must be 1"
-        if self.with_bias:
-            e, theta = x[-self.m_dim :], x[0 : self.n_dim]
-            f_value = self._npdf(theta)
-            bias = self.bias_param * f_value
-            return np.linalg.norm(e - bias) < f_value
-        else:
-            return np.linalg.norm(x[-self.m_dim :]) < self._npdf(x[0 : self.n_dim])
+        e, theta = x[-self.m_dim :], x[0 : self.n_dim]
+        f_value = self._npdf(theta)
+        bias = self.bias_param * f_value
+        inside_outer = bool(np.linalg.norm(e - bias) < f_value)
+        inside_inner = bool(np.linalg.norm(e - bias) < self.hollow_scale * f_value)
+        return inside_outer and not inside_inner
 
     def default_init_param(self) -> np.ndarray:
         return np.array([-2.0] + [0 for i in range(self.n_dim - 1)])
@@ -55,16 +59,24 @@ class GaussianEnvironment:
 
     def visualize_region(self, param_min, param_max, fax) -> None:
         param_lin = np.linspace(param_min, param_max, 200)
-        if self.with_bias:
-            f_value = np.exp(-0.5 * param_lin**2)
-            bias = self.bias_param * f_value
-            upper = f_value + bias
-            lower = -f_value + bias
-        else:
-            upper = np.exp(-0.5 * param_lin**2)
-            lower = -upper
+        f_value = np.exp(-0.5 * param_lin**2)
+        bias = self.bias_param * f_value
+        upper_outer = f_value + bias
+        lower_outer = -f_value + bias
+        upper_inner = self.hollow_scale * f_value + bias
+        lower_inner = -self.hollow_scale * f_value + bias
         fig, ax = fax
-        ax.fill_between(param_lin, lower, upper, color="black", alpha=0.15, edgecolor="white")
+        if self.with_hollow:
+            ax.fill_between(
+                param_lin, upper_inner, upper_outer, color="black", alpha=0.15, edgecolor="white"
+            )
+            ax.fill_between(
+                param_lin, lower_outer, lower_inner, color="black", alpha=0.15, edgecolor="white"
+            )
+        else:
+            ax.fill_between(
+                param_lin, lower_outer, upper_outer, color="black", alpha=0.15, edgecolor="white"
+            )
 
     def visualize_optimal_sliced_region(self, fax) -> None:
         fig, ax = fax
